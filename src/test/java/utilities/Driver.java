c8b1b9943fc76300ec28590b3edd74c59f394105
azizshn <azizsahin@yandex.com>
2022-01-14 13:54:02 +0000

    Commented out the window manager

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -61,7 +61,7 @@ public class Driver {
         }
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       // driver.manage().window().maximize();
+      //  driver.manage().window().maximize();
 
         return driver;
     }
